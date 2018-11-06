a6f3eb8c1078f21be46d4cd4596f7b8ed203c76f
MaksGostroushko <32840948+MaksGostroushko@users.noreply.github.com>
2018-11-06 10:43:27 +0200

    Update TestSuit

--- lib/test_suit.rb
+++ lib/test_suit.rb
@@ -1,29 +1,17 @@
-require "test_suit/version"
+require_relative "test_suit/version"
 
 module TestSuite
-  # result == hashes
+
+  def self.to_f(actual)
+    puts "Element now is integer(float)"
+    puts "It is #{actual.to_f} "
+  end
+
   def self.hash_empty?(actual)
     puts actual.empty? ? "Hash is empty" : "Hash is not empty"
     puts "Hash: #{actual}"
   end
 
-  def self.hash_result_greater_nil?(actual)
-    arr = []
-    actual.each_result { |res| arr << res if res > 0 }
-    puts arr.size == actual.size ? "All hash results are greater than 0" : "Hash has result equar||less than nil"
-    puts "Hash result: #{actual.result}"
-  end
-
-  def self.hash_has_two_keys?(actual)
-    if actual.empty?
-      puts "Hash is empty"
-    else
-      puts actual.size > 1 ? "Hash has 2 or more keys" : "Hash has one key"
-      puts "Hash key(s): #{actual.keys}"
-    end
-  end
-
-  # Type == object
    def self.number?(actual)
     puts actual.is_a?(Numeric) ? "Object is a number" : "Object isn't a number"
     puts "Object: #{actual}"
@@ -33,7 +21,7 @@ module TestSuite
     puts actual.is_a?(String) ? "Object is a string" : "Object isn't a string"
     puts "Object: #{actual}"
   end
-  
+
   def self.hash?(actual)
     puts actual.is_a?(Hash) ? "Object is a hash" : "Object isn't a hash"
     puts "Object: #{actual}"
@@ -46,28 +34,36 @@ module TestSuite
 
   def self.symbol?(actual)
     puts actual.is_a?(Symbol) ? "Object is a symbol" : "Object is not a symbol"
-    puts "Object: #{actual}"
+    puts "Element: #{actual}"
   end
 
-  # Type == to_s,_i,_sym,_arr
   def self.to_s(actual)
-    puts "Object converted to string"
+    puts "Element now is string"
     puts "It is #{actual.to_s.inspect} "
   end
 
   def self.to_i(actual)
-    puts "Object converted to num"
+    puts "Element now is num"
     puts "It is #{actual.to_i} "
   end
 
   def self.to_sym(actual)
     actual = actual.to_s if actual.is_a?(Numeric)
-    puts "Object converted to sym"
+    puts "Element now is sym"
     puts "It is #{actual.to_sym.inspect} "
   end
 
-  def self.str_to_arr(actual)
-    puts "Object converted to array"
+  def self.str_arr(actual)
+    puts "Element now is array"
     puts "It is #{actual.split.inspect}"
+  end
+
+  def self.how_hash_hase_results(actual)
+    if actual.empty?
+      puts "Hash is empty"
+    else
+      puts actual.size > 1 ? "Hash has 2 or more keys" : "Hash has one key"
+      puts "Hash key(s): #{actual.keys}"
+    end
   end
 end
